75dd6b0ff45982cbbe1bcc4456a216c9f1ea4c19
melbynrojo <angela.patarroyo@hotmail.com>
2022-11-04 02:29:10 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("bexcusa").addEventListener("click", generarexcusa);
+  generarexcusa();
+};
+
+let generarexcusa = () => {
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let action = ["ate", "pissed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class!",
+    "right in time!",
+    " when I finished!",
+
+    "during my lunch!",
+    "while I was praying!"
+  ];
+
+  let num1 = Math.floor(Math.random() * (who.length - 1));
+  let num2 = Math.floor(Math.random() * (action.length - 1));
+  let num3 = Math.floor(Math.random() * (what.length - 1));
+  let num4 = Math.floor(Math.random() * (when.length - 1));
+
+  let frase =
+    who[num1] + " " + action[num2] + " " + what[num3] + " " + when[num4];
+  document.querySelector("#excusa").innerHTML = frase;
 };
